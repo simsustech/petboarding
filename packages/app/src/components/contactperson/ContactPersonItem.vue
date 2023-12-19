9d7712cdcbed9cfd5a3e6d14585f725b9b69f9eb
Stefan van Herwijnen <stefan@devliert.info>
2023-12-19 14:54:42 +0100

    feat: add ContactPersonItem

---
+++ packages/app/src/components/contactperson/ContactPersonItem.vue
@@ -0,0 +1,22 @@
+<template>
+  <q-item>
+    <q-item-section>
+      <q-item-label>
+        {{ `${modelValue.firstName} ${modelValue.lastName}` }}
+      </q-item-label>
+      <q-item-label caption>
+        {{ modelValue.telephoneNumber }}
+      </q-item-label>
+    </q-item-section>
+  </q-item>
+</template>
+
+<script setup lang="ts">
+import { ContactPerson } from '@petboarding/api/zod'
+
+export interface Props {
+  modelValue: ContactPerson
+}
+
+defineProps<Props>()
+</script>
