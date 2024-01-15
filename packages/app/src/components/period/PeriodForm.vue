94f87c6095e623497859ae185a838716a22db037
Stefan van Herwijnen <stefan@devliert.info>
2024-01-15 14:12:00 +0100

    feat: change DateInputs to QDate in PeriodForm

--- packages/app/src/components/period/PeriodForm.vue
+++ packages/app/src/components/period/PeriodForm.vue
@@ -1,64 +1,49 @@
 <template>
   <q-form ref="formRef">
     <div class="row q-col-gutter-sm">
-      <date-input
-        v-model="modelValue.startDate"
-        :label="lang.period.fields.startDate"
-        clearable
-        class="col-md-6 col-12"
-        :date="{
-          noUnset: true,
-          firstDayOfWeek: '1',
-          options: limitDateOptionsFn
-        }"
+      <q-date
+        v-model="dateRange"
+        class="col-md-5 col-12"
+        first-day-of-week="1"
+        range
+        :options="limitDateOptionsFn"
+        @update:model-value="removeDates"
+        @range-end="setDates"
       />
-      <date-input
-        v-model="modelValue.endDate"
-        :label="lang.period.fields.endDate"
-        clearable
-        required
-        class="col-md-6 col-12"
-        :date="{
-          noUnset: true,
-          firstDayOfWeek: '1',
-          options: limitDateOptionsFn
-        }"
-      />
-
-      <period-type-select
-        v-model="modelValue.type"
-        :label="lang.period.fields.type"
-      />
-    </div>
-    <div class="row">
-      <form-input
-        v-bind="input"
-        id="comments"
-        v-model="modelValue.comments"
-        class="col-12"
-        name="comments"
-        :label="lang.period.fields.comments"
-        bottom-slots
-        lazy-rules
-        type="textarea"
-        rows="3"
-      />
-      <div v-show="false" class="col-12">
-        <div class="row">
-          {{ lang.period.fields.minimumRatingForException }}
-        </div>
-        <div class="row">
-          <q-rating
-            v-if="modelValue.minimumRatingForException !== void 0"
-            :model-value="modelValue.minimumRatingForException || 0"
-            size="3em"
-            icon="star_border"
-            icon-selected="star"
-            icon-half="star_half"
-            @update:model-value="
-              ($event) => (modelValue.minimumRatingForException = $event)
-            "
-          />
+      <div class="col col-md-6 col-12">
+        <period-type-select
+          v-model="modelValue.type"
+          :label="lang.period.fields.type"
+        />
+        <form-input
+          v-bind="input"
+          id="comments"
+          v-model="modelValue.comments"
+          class="col-12"
+          name="comments"
+          :label="lang.period.fields.comments"
+          bottom-slots
+          lazy-rules
+          type="textarea"
+          rows="3"
+        />
+        <div v-show="false" class="col-12">
+          <div class="row">
+            {{ lang.period.fields.minimumRatingForException }}
+          </div>
+          <div class="row">
+            <q-rating
+              v-if="modelValue.minimumRatingForException !== void 0"
+              :model-value="modelValue.minimumRatingForException || 0"
+              size="3em"
+              icon="star_border"
+              icon-selected="star"
+              icon-half="star_half"
+              @update:model-value="
+                ($event) => (modelValue.minimumRatingForException = $event)
+              "
+            />
+          </div>
         </div>
       </div>
     </div>
@@ -73,10 +58,10 @@ export default {
 
 <script setup lang="ts">
 import { ref } from 'vue'
-import { extend, QForm, date as dateUtil } from 'quasar'
+import { extend, QDate, QForm, date as dateUtil } from 'quasar'
 import { useLang } from '../../lang/index.js'
 import { ResponsiveDialog } from '@simsustech/quasar-components'
-import { DateInput, FormInput } from '@simsustech/quasar-components/form'
+import { FormInput } from '@simsustech/quasar-components/form'
 import { Period, PERIOD_TYPE } from '@petboarding/api/zod'
 import PeriodTypeSelect from './PeriodTypeSelect.vue'
 import type { QFormProps, QInputProps } from 'quasar'
@@ -127,6 +112,41 @@ const modelValue = ref<Period>(initialValue)
 
 const setValue = (newValue: Period) => {
   modelValue.value = extend({}, initialValue, newValue)
+  dateRange.value = {
+    from: newValue.startDate.replaceAll('-', '/'),
+    to: newValue.endDate.replaceAll('-', '/')
+  }
+}
+
+const dateRange = ref({
+  from: '',
+  to: ''
+})
+
+const setDates: InstanceType<typeof QDate>['$props']['onRangeEnd'] = ({
+  from,
+  to
+}) => {
+  const startDate = `${from.year}/${from.month}/${from.day}`
+  const endDate = `${to.year}/${to.month}/${to.day}`
+  if (startDate !== endDate) {
+    modelValue.value.startDate = startDate
+    modelValue.value.endDate = endDate
+  } else {
+    dateRange.value = {
+      from: '',
+      to: ''
+    }
+  }
+}
+
+const removeDates: InstanceType<
+  typeof QDate
+>['$props']['onUpdate:modelValue'] = (value, reason, details) => {
+  if (reason === 'remove-range') {
+    modelValue.value.startDate = ''
+    modelValue.value.endDate = ''
+  }
 }
 
 const submit: InstanceType<typeof ResponsiveDialog>['$props']['onSubmit'] = ({
