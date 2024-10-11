a44953c18376c90870195ccf963a97a2725b4dd8
Stefan van Herwijnen <stefan@devliert.info>
2024-10-11 18:51:59 +0200

    fix: nl booking cancel template

--- packages/api/src/templates/email/bookings/cancel/nl.ts
+++ packages/api/src/templates/email/bookings/cancel/nl.ts
@@ -2,9 +2,19 @@ import { c } from 'compress-tag'
 // import env from '@vitrify/tools/env'
 // const hostname = env.read('VITE_API_HOSTNAME') || env.read('API_HOSTNAME')
 
-const subject = c`Met betrekking tot uw reservering.`
-const body = c`<p>
-  Beste {{customer.firstName}} {{customer.lastName}},
+const subject = c`Uw reservering is geannuleerd.`
+const body = c`
+  <p>
+      Beste {{customer.firstName}} {{customer.lastName}},
+  </p>
+  <p>
+      Deze email is om u te informeren dat uw reservering van
+      <b>{{startDate}} {{startTime}}</b> tot en met
+      <b>{{endDate}} {{endTime}}</b> voor uw huisdieren
+      <b>{{pets}}</b>
+      is geannuleerd om de volgende reden:
+      {{reason}}.
+      Houdt rekening met eventuele annuleringskosten.
   </p>`
 
 export { subject, body }
