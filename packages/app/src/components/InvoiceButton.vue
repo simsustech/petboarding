9e7b407f417ba7be6982b65cb8b7665da9fc2636
Stefan van Herwijnen <stefan@devliert.info>
2024-10-14 15:49:41 +0200

    feat(InvoiceButton): add check if amountDue <= 0, print on html2canvas

--- packages/app/src/components/InvoiceButton.vue
+++ packages/app/src/components/InvoiceButton.vue
@@ -1,20 +1,38 @@
 <template>
   <q-btn
     :dense="$q.screen.lt.sm"
-    class="q-pt-none q-pb-none"
-    data-html2canvas-ignore="true"
+    class="q-pa-none"
     :href="`https://${configuration.INTEGRATIONS?.slimfact.hostname}/invoice/${modelValue.uuid}`"
     target="_blank"
     :text-color="invoiceTextColor"
   >
     <div class="column">
-      <q-icon class="col-12" name="receipt" />
-      <div class="col-12 text-caption">
-        <price
-          :model-value="modelValue.totalIncludingTax || 0"
-          :currency="modelValue.currency"
-        />
-      </div>
+      <q-avatar class="col-12">
+        <q-icon name="receipt" size="md" />
+      </q-avatar>
+      <q-icon
+        v-if="
+          modelValue.amountDue !== void 0 &&
+          modelValue.amountDue !== null &&
+          modelValue.amountDue <= 0
+        "
+        style="
+          position: relative;
+          width: 0;
+          height: 0;
+          right: -30px;
+          bottom: 3px;
+        "
+        name="check"
+        color="green"
+        size="xs"
+      />
+    </div>
+    <div class="col-12 text-caption">
+      <price
+        :model-value="modelValue.totalIncludingTax || 0"
+        :currency="modelValue.currency"
+      />
     </div>
     <q-tooltip>
       {{ lang.booking.messages.openInvoice }}
@@ -39,7 +57,10 @@ const lang = useLang()
 const configuration = useConfiguration()
 
 const invoiceTextColor = computed(() => {
-  if (modelValue.value.amountDue !== void 0) {
+  if (
+    modelValue.value.amountDue !== void 0 &&
+    modelValue.value.amountDue !== null
+  ) {
     if (modelValue.value.amountDue <= 0) return 'green-6'
     if (
       modelValue.value.amountPaid &&
