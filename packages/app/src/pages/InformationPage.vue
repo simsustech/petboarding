454434724da22884096f2a3c26edbeaa683c02e9
Stefan van Herwijnen <stefan@devliert.info>
2025-06-18 13:20:09 +0200

    fix: fix InformationPage padding

--- packages/app/src/pages/InformationPage.vue
+++ packages/app/src/pages/InformationPage.vue
@@ -1,30 +1,32 @@
 <template>
-  <q-banner rounded>
-    <template #avatar>
-      <q-icon name="i-mdi-warning" color="warning" />
-    </template>
-    <a href="/print/termsandconditions" target="_blank">{{
-      lang.information.messages.termsAndConditions
-    }}</a>
-  </q-banner>
-  <q-banner rounded>
-    <template #avatar>
-      <q-icon name="i-mdi-warning" color="warning" />
-    </template>
-    <a>{{ lang.information.messages.petHealth }}</a>
-  </q-banner>
-  <q-banner rounded>
-    <template #avatar>
-      <q-icon name="i-mdi-info" color="info" />
-    </template>
-    <a>{{ lang.information.messages.openingTimes }}</a>
-  </q-banner>
-  <q-banner rounded class="bg-red-2">
-    <template #avatar>
-      <q-icon name="i-mdi-exclamation" color="red" />
-    </template>
-    <a>{{ lang.information.messages.vaccinations }}</a>
-  </q-banner>
+  <q-page padding>
+    <q-banner rounded>
+      <template #avatar>
+        <q-icon name="i-mdi-warning" color="warning" />
+      </template>
+      <a href="/print/termsandconditions" target="_blank">{{
+        lang.information.messages.termsAndConditions
+      }}</a>
+    </q-banner>
+    <q-banner rounded>
+      <template #avatar>
+        <q-icon name="i-mdi-warning" color="warning" />
+      </template>
+      <a>{{ lang.information.messages.petHealth }}</a>
+    </q-banner>
+    <q-banner rounded>
+      <template #avatar>
+        <q-icon name="i-mdi-info" color="info" />
+      </template>
+      <a>{{ lang.information.messages.openingTimes }}</a>
+    </q-banner>
+    <q-banner rounded class="bg-red-2">
+      <template #avatar>
+        <q-icon name="i-mdi-exclamation" color="red" />
+      </template>
+      <a>{{ lang.information.messages.vaccinations }}</a>
+    </q-banner>
+  </q-page>
 </template>
 
 <script setup lang="ts">
